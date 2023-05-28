56d359ebc635ed4435e74136c8d46c57920064bb
Doni Putra <d.putra@global-coding.com>
2023-05-28 18:33:39 +0700

    feat : add news endpoint for public access

---
+++ src/controllers/news.js
@@ -0,0 +1,27 @@
+// Importing the dependencies
+import * as model from '../models/news.js';
+import { APIResponse } from '../utils/helper.js';
+
+export async function getNews(req, res, next) {
+    model.getNews().then(result => {
+        APIResponse(res, "Successfully get all news", 200, true, result);
+    }).catch(err => {
+        APIResponse(res, "Failed get all news", 500, true, err);
+    });
+}
+
+export async function getNewsById(req, res, next) {
+    // Get id on params
+    let id = req.params.newsId
+
+    model.getNewsById(id).then(result => {
+        // Check found the data or not
+        if (result.length > 0) {
+            APIResponse(res, "Successfully get news by id", 200, true, result);
+        } else {
+            APIResponse(res, "News data not found !", 404, true, result);
+        }
+    }).catch(err => {
+        APIResponse(res, "Failed get news by id", 500, true, err);
+    });
+}
